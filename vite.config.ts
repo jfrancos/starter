a5c7b15d2b9df62d5c39b5d3914613cab5819022
Justin Francos <justinfrancos@gmail.com>
2026-03-10 09:13:35 -0400

    updtes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import unoCSS from "unocss/vite";
+import { presetWind4 } from "unocss";
+
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [svelte(), unoCSS()],
+	plugins: [svelte(), unoCSS({ presets: [presetWind4()] })],
 });
