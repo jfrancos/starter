7a965b918bc7bfc7eb1d938c247979b477e32dfd
Justin Francos <justinfrancos@gmail.com>
2026-03-15 10:57:46 -0400

    incorporate shadcn wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { presetWind4 } from "unocss";
 import unoCSS from "unocss/vite";
+import presetAnimations from "unocss-preset-animations";
+import { presetShadcn } from "unocss-preset-shadcn";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte(), unoCSS({ presets: [presetWind4()] })],
+	plugins: [
+		svelte(),
+		unoCSS({ presets: [presetWind4, presetAnimations, presetShadcn] }),
+	],
 });
